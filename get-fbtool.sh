632cd9df27942ee7f0f681601e2365930cb3334d
Howard Chen <howard.chen@linaro.org>
2015-05-08 15:23:53 +0800

    Firmware Update 20150508
        - Support C1 state
        - Add recovery tools

---
+++ get-fbtool.sh
@@ -0,0 +1,6 @@
+pwd=`pwd`
+git clone https://github.com/mtk09422/chromiumos-third_party-mediatek-datool ../fbtool
+cd ../fbtool
+make
+cp fbtool $pwd
+cd -
---
+++ update-recover.sh
@@ -0,0 +1,1 @@
+sudo ./fbtool preloader_8173.bin lk_8173.bin
